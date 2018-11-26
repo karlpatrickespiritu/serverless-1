ab8c9a045933f9438fc8a3c607ec005b5ad1345a
karlpatrickespiritu <wiwa.espiritu@gmail.com>
2018-11-26 16:50:56 +0800

    test

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"encoding/json"
 )
 
 type MyEvent struct {
@@ -11,6 +12,13 @@ type MyEvent struct {
 }
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	out, _ := json.MarshalIndent(name, " ", "\t")
+	fmt.Println("\n name", string(out))
+
+	two, _ := json.MarshalIndent(ctx, " ", "\t")
+	fmt.Println("\n ctx", string(two))
+
+
 	return fmt.Sprintf("Hello %s!", name.Name), nil
 }
 
